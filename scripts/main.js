2685c3902073fe63efd220682cd9086e1f8516a9
VLI77 <vincent.li@hetic.net>
2020-01-03 14:03:44 +0100

    royal noisiel
    royal noisiel

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+let menuMobile = document.querySelector('.menu_responsive_hidden')
+let buttonHamburger = document.querySelector('#logo_hamburger')
+let info = document.querySelector('.info')
+
+
+
+buttonHamburger.addEventListener('click',function(){
+
+if (menuMobile.classList.contains("menu_responsive_visible")) {
+    menuMobile.classList.remove('menu_responsive_visible')
+    menuMobile.classList.remove('display')
+
+    info.classList.add('info')
+    console.log("ok");
+  }
+else {
+  menuMobile.classList.add('menu_responsive_visible')
+  menuMobile.classList.add("display")
+  info.classList.remove('info')
+}
+})
